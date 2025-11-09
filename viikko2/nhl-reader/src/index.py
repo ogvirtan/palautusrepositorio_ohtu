d49ff9c3eb6d0b159ae115d91058300c5ad5eb3a
Olli Virtanen <ovirtanen96@gmail.com>
2025-11-09 15:48:08 +0200

    testataan precommit-hookkia pt3

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -56,7 +56,7 @@ def nationality_loop(stats, reader, console, season):
         players_table(stats, nationality, console, season)
 
 
-def players_table(stats, nationality, console, season):# pylint: disable=too-many-statemen
+def players_table(stats, nationality, console, season):# pylint: disable=too-many-stateme
     table = Table(show_footer=False)
     table_left = Align.left(table)
 
