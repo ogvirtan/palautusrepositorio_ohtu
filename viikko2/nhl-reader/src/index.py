b1c2519a9dae6e22e31bf82d03794b1555f07c58
Olli Virtanen <ovirtanen96@gmail.com>
2025-11-09 15:54:30 +0200

    pre-commit hook kunnossa

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -56,7 +56,7 @@ def nationality_loop(stats, reader, console, season):
         players_table(stats, nationality, console, season)
 
 
-def players_table(stats, nationality, console, season):# pylint: disable=too-many-state
+def players_table(stats, nationality, console, season):# pylint: disable=too-many-statements
     table = Table(show_footer=False)
     table_left = Align.left(table)
 
